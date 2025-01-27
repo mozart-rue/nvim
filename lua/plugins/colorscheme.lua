db8a2615bc19075b191e96e6dd9ba306d45b7d4d
mozart-rue <mozartrue@outlook.com>
2025-01-27 00:21:46 -0300

    feat: add some colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -47,6 +47,112 @@ return {
 	},
 
 	{
+		"projekt0n/github-nvim-theme",
+		lazy = true,
+		priority = 1000,
+		config = function()
+			require("github-theme").setup({
+				options = {
+					compile_path = vim.fn.stdpath("cache") .. "/github-theme",
+					compile_file_suffix = "_compiled", -- Compiled file suffix
+					hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
+					hide_nc_statusline = true, -- Override the underline style for non-active statuslines
+					transparent = true, -- Disable setting bg (make neovim's background transparent)
+					terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+					dim_inactive = false, -- Non focused panes set to alternative background
+					module_default = true, -- Default enable value for modulesitalic
+					styles = { -- Style to be applied to different syntax groups
+						comments = "italic", -- Value is any valid attr-list value `:help attr-list`
+						functions = "underdotted",
+						keywords = "NONE",
+						variables = "bold",
+						conditionals = "standout",
+						constants = "NONE",
+						numbers = "bold",
+						operators = "NONE",
+						strings = "undercurl",
+						types = "NONE",
+					},
+					inverse = { -- Inverse highlight for different types
+						match_paren = false,
+						visual = false,
+						search = false,
+					},
+					darken = { -- Darken floating windows and sidebar-like windows
+						floats = true,
+						sidebars = {
+							enable = true,
+							list = {}, -- Apply dark background to specific windows
+						},
+					},
+					modules = { -- List of various plugins and additional options
+						-- ...
+					},
+				},
+				palettes = {},
+				specs = {},
+				groups = {},
+			})
+		end,
+	},
+
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "auto", -- latte, frappe, macchiato, mocha
+				background = { -- :h background
+					light = "latte",
+					dark = "mocha",
+				},
+				transparent_background = true, -- disables setting the background color.
+				show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+				term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+				dim_inactive = {
+					enabled = false, -- dims the background color of inactive window
+					shade = "dark",
+					percentage = 0.15, -- percentage of the shade to apply to the inactive window
+				},
+				no_italic = false, -- Force no italic
+				no_bold = false, -- Force no bold
+				no_underline = false, -- Force no underline
+				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+					comments = { "italic" }, -- Change the style of comments
+					conditionals = { "italic" },
+					loops = {},
+					functions = {},
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+					operators = {},
+					-- miscs = {}, -- Uncomment to turn off hard-coded styles
+				},
+				color_overrides = {},
+				custom_highlights = {},
+				default_integrations = true,
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = false,
+					mini = {
+						enabled = true,
+						indentscope_color = "",
+					},
+					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+				},
+			})
+		end,
+	},
+
+	{
 		"scottmckendry/cyberdream.nvim",
 		lazy = true,
 		priority = 1000,
