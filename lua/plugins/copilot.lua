d2e36389bf673387eb7e21e6379e93109db9f3e5
mozart-rue <mozartrue@outlook.com>
2025-01-27 00:22:30 -0300

    feat: disable panel and suggestion to avoid conflit with copilot-cmp

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,7 +6,7 @@ return {
 		config = function()
 			require("copilot").setup({
 				panel = {
-					enabled = true,
+					enabled = false,
 					auto_refresh = false,
 					keymap = {
 						jump_prev = "[[",
@@ -21,8 +21,8 @@ return {
 					},
 				},
 				suggestion = {
-					enabled = true,
-					auto_trigger = false,
+					enabled = false,
+					auto_trigger = true,
 					hide_during_completion = true,
 					debounce = 75,
 					keymap = {
