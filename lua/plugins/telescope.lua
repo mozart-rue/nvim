61bb8295443b0d9d11190f197aa2929ef0230ea9
Mozart <mozart@toteduca.com>
2025-02-27 09:53:08 -0300

    feat: set keymap for gitfiles

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -79,6 +79,7 @@ return {
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+			vim.keymap.set("n", "<leader>sp", builtin.git_files, { desc = "[sp] search for git files" })
 
 			vim.keymap.set("n", "<leader>;f", function()
 				local telescope = require("telescope")
