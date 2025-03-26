afa87c4ee8c9c6582bb197d295dd86635cff4383
Mozart <mozart@toteduca.com>
2025-03-26 18:40:45 -0300

    feat: set eslint and prettier

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -34,7 +34,18 @@ return {
 			--  into multiple repos for maintenance purposes.
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
+			"tailwind-tools",
+			"onsails/lspkind.nvim",
 		},
+		opts = function()
+			return {
+				formatting = {
+					format = require("lspkind").cmp_format({
+						before = require("tailwind-tools.cmp").lspkind_format,
+					}),
+				},
+			}
+		end,
 		config = function()
 			-- See `:help cmp`
 			local cmp = require("cmp")
