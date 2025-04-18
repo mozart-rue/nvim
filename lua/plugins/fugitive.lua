117055daeff62c009662126613a4475868d5edf6
Mozart Lucas Rueckert <mozartrue@outloook.com>
2025-04-18 13:41:44 -0300

    feat: add some extras plugins

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,40 @@
+return {
+  "tpope/vim-fugitive",
+  config = function()
+    vim.keymap.set("n", "<leader>fu", vim.cmd.G)
+
+    local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+
+    local autocmd = vim.api.nvim_create_autocmd
+    autocmd("BufWinEnter", {
+      group = ThePrimeagen_Fugitive,
+      pattern = "*",
+      callback = function()
+        if vim.bo.ft ~= "fugitive" then
+          return
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = { buffer = bufnr, remap = false }
+        vim.keymap.set("n", "<leader>p", function()
+          vim.cmd.Git("push")
+        end, opts)
+
+        -- rebase always
+        vim.keymap.set("n", "<leader>P", function()
+          vim.cmd.Git({ "pull" })
+        end, opts)
+
+        -- View commit history of the entire repo
+        vim.keymap.set("n", "<leader>gL", ":G log<CR>", { desc = "Repository commit history" })
+
+        -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+        -- needed if i did not set the branch up correctly
+        vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+      end,
+    })
+
+    vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+    vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+  end,
+}
